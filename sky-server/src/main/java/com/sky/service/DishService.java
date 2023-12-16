41b3f8c90a2d80781a2fecdd15d6083961be3ffc
yinhao <yinhao023h@gmail.com>
2023-12-16 17:26:29 +0800

    套餐管理接口

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -44,4 +44,11 @@ public interface DishService {
      */
 
     void updateWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId
+     * @return
+     */
+    List<Dish> list(Long categoryId);
 }
